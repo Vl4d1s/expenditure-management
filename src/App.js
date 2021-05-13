db2385dd83d1e337f0e0c4dd44fbd5a105ae4525
Vl4d1s <vladismarkin@gmail.com>
2021-05-13 21:27:23 +0300

    working on the ExpenseItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <div>March 28th 2021</div>
+      <div>
+        <h2>Car Insurencs</h2>
+        <div>$294.67</div>
+      </div>
     </div>
   );
 }
