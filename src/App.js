2252a8445f5d8d079cfb4ba27a7c7c358962e2bf
Vl4d1s <vladismarkin@gmail.com>
2021-05-13 21:38:29 +0300

    adding css to our ExpenseItem Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
-  return (
-    <div>
-      <div>March 28th 2021</div>
-      <div>
-        <h2>Car Insurencs</h2>
-        <div>$294.67</div>
-      </div>
-    </div>
-  );
+  return <ExpenseItem></ExpenseItem>;
 }
 
 export default App;
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./ExpenseItem.css";
 
 const ExpenseItem = () => {
-  return <div>ExpenseItem</div>;
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurencs</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 };
 
 export default ExpenseItem;
