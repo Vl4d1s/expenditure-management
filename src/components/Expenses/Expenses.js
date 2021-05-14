8e09283ec16d4be279e844ff1775af2d07e7a6e9
Vl4d1s <vladismarkin@gmail.com>
2021-05-14 15:02:41 +0300

    two way binding chhosen filter state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,17 @@
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./ExpensesFilter.css";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   const renderedItems = items.map((item) => {
     return (
       <ExpenseItem
@@ -14,7 +23,15 @@ const Expenses = ({ items }) => {
     );
   });
 
-  return <Card className="expenses">{renderedItems}</Card>;
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {renderedItems}
+    </Card>
+  );
 };
 
 export default Expenses;
